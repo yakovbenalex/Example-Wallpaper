f035c68025e0313122b2c53c0aabaee8fb467678
yakovbenalex <yakovbenalex@mail.ru>
2022-07-22 01:06:57 +0300

    Initial commit

---
+++ app/src/main/java/ru/jacobo/example_wallpapers/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package ru.jacobo.example_wallpapers.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.jacobo.example_wallpapers.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
